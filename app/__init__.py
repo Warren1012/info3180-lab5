2ebf97cc2effdf802f47b0289bb101bb15cb331b
Annwar Greenland <annwar101@gmail.com>
2022-03-01 14:53:21 -0500

    Password field Added

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from .config import Config
 from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy(app)
 
@@ -13,6 +14,11 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+def __init__(self, first_name, last_name, username, password):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.username = username 
+    self.password = generate_password_hash(password, method='pbkdf2:sha256')
 # Instantiate Flask-Migrate library here
 migrate = Migrate(app, db)
 
